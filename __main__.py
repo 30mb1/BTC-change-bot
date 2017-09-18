3add120a564cada4adb1412f493dd63db40e3bde
AlievMagomed <steam.30mb1@gmail.com>
2017-09-18 18:29:41 +0300

    /

--- __main__.py
+++ __main__.py
@@ -17,7 +17,7 @@ from database import users
 from ast import literal_eval
 
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger('Main')
 
 MENU, WITHDRAW = range(2)
---
+++ trade/admin.py
@@ -0,0 +1,5 @@
+from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove, ParseMode, InlineKeyboardButton,
+                    InlineKeyboardMarkup, ParseMode)
+import texts
+from database import users, pay_systems
+from decimal import *
