6ceee3df0ce9b12d5c55937d884b5e4a62bf6c07
pavlovdog <pavlovdog.moscow@gmail.com>
2018-12-12 18:52:19 +0300

    Fixed imports

--- __main__.py
+++ __main__.py
@@ -1,10 +1,17 @@
 import logging
-from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove, ParseMode, InlineKeyboardButton,
-                    InlineKeyboardMarkup, ParseMode)
+from telegram import ReplyKeyboardMarkup
+from telegram import ReplyKeyboardRemove
+from telegram import ParseMode
+from telegram import InlineKeyboardButton
+from telegram import InlineKeyboardMarkup
+from telegram import ParseMode
+
 from telegram.ext import (Updater, ConversationHandler, RegexHandler, CommandHandler, MessageHandler,
-                        Filters, CallbackQueryHandler)
+                          Filters, CallbackQueryHandler)
+
 from telegram.error import (TelegramError, Unauthorized, BadRequest,
                             TimedOut, ChatMigrated, NetworkError)
+
 from telegram.ext.dispatcher import run_async
 from wallet import wallet as w
 from trade import trade as t
@@ -17,7 +24,8 @@ import json
 from utils.router import query_route
 
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger('Main')
 
 MENU, WITHDRAW, CHOOSE_TYPE, PAY_SYSTEM, RATE, LIMMITS, DESCRIPTION = range(7)
@@ -29,7 +37,7 @@ def error_callback(bot, update, error):
     try:
         raise error
     except Exception as e:
-        print (e)
+        print(e)
 
 
 def start(bot, update, user_data):
@@ -65,41 +73,41 @@ def main():
 
         states={
 
-            #regular expression for all languages should be here.
-            MENU : [
+            # regular expression for all languages should be here.
+            MENU: [
                 RegexHandler('^(💰 Кошелек)$', w.show_wallet, pass_user_data=True),
                 RegexHandler('^(📊 Купить/продать)$', t.show_trade, pass_user_data=True),
                 RegexHandler('^(🔩 Настройки)$', i.show_instruments, pass_user_data=True),
                 RegexHandler('^(ℹ О сервисе)$', i.about_us, pass_user_data=True)
             ],
 
-            WITHDRAW : [
+            WITHDRAW: [
                 RegexHandler('^(Отмена)$', cancel, pass_user_data=True),
                 MessageHandler(Filters.text, transfer.withdraw, pass_user_data=True)
             ],
 
-            #states for creating new order
-            CHOOSE_TYPE : [
+            # states for creating new order
+            CHOOSE_TYPE: [
                 RegexHandler('^(Отмена)$', cancel, pass_user_data=True),
                 MessageHandler(Filters.text, a.create.get_type, pass_user_data=True)
             ],
 
-            PAY_SYSTEM : [
+            PAY_SYSTEM: [
                 RegexHandler('^(Назад)$', a.create.create_order, pass_user_data=True),
                 MessageHandler(Filters.text, a.create.get_pay_system, pass_user_data=True)
             ],
 
-            RATE : [
+            RATE: [
                 RegexHandler('^(Назад)$', a.create.get_type, pass_user_data=True),
                 MessageHandler(Filters.text, a.create.get_rate, pass_user_data=True)
             ],
 
-            DESCRIPTION : [
+            DESCRIPTION: [
                 RegexHandler('^(Назад)$', a.create.get_pay_system, pass_user_data=True),
                 MessageHandler(Filters.text, a.create.get_description, pass_user_data=True)
             ],
 
-            LIMMITS : [
+            LIMMITS: [
                 RegexHandler('^(Назад)$', a.create.get_pay_system, pass_user_data=True),
                 MessageHandler(Filters.text, transfer.withdraw, pass_user_data=True)
             ]
@@ -109,10 +117,10 @@ def main():
         allow_reentry=True
     )
 
-
     dispatcher.add_handler(main_menu)
     updater.start_polling()
     # updater.idle()
+
 
 if __name__ == '__main__':
     main()
